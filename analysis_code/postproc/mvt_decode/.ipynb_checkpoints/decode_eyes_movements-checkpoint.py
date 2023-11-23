38ec6d8efb78765b3fe5a5c4a57a6bc729e7d4ce
Uriel LASCOMBES uriel.lascombes@etu.univ-amu.fr <ulascombes@login02.cluster>
2023-11-23 14:58:59 +0100

    work on fit

---
+++ analysis_code/postproc/mvt_decode/.ipynb_checkpoints/decode_eyes_movements-checkpoint.py
@@ -0,0 +1,207 @@
+"""
+-----------------------------------------------------------------------------------------
+decode_eyes_movements.py
+-----------------------------------------------------------------------------------------
+Goal of the script:
+decode eyes movements or not
+-----------------------------------------------------------------------------------------
+Input(s):
+sys.argv[1]: main project directory
+sys.argv[2]: project name (correspond to directory)
+sys.argv[3]: subject name
+sys.argv[4]: group of shared data (e.g. 327)
+-----------------------------------------------------------------------------------------
+Output(s):
+# Preprocessed and averaged timeseries files
+-----------------------------------------------------------------------------------------
+To run:
+1. cd to function
+>> cd ~/projects/RetinoMaps/analysis_code/postproc/mvt_decode/
+2. run python command
+python preproc_end.py [main directory] [project name] [subject name] [group]
+-----------------------------------------------------------------------------------------
+Exemple:
+python decode_eyes_movements.py /scratch/mszinte/data RetinoMaps sub-02 327
+-----------------------------------------------------------------------------------------
+Written by Uriel Lascombes (uriel.lascombes@univ-amu.fr)
+-----------------------------------------------------------------------------------------
+"""
+# General import 
+import os
+import nibabel as nb
+import sys
+
+
+import numpy as np
+
+
+
+import json
+import glob
+import pandas as pd
+from scipy.stats import loguniform, randint
+
+
+import warnings
+warnings.filterwarnings("ignore")
+
+#sklearn import
+
+from sklearn.model_selection import GridSearchCV
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+
+
+from sklearn.model_selection import RandomizedSearchCV
+
+
+
+
+main_dir = sys.argv[1]
+project_dir = sys.argv[2]
+subject = sys.argv[3]
+group = sys.argv[4]
+
+with open('../../settings.json') as f:
+    json_s = f.read()
+    analysis_info = json.loads(json_s)
+TR = analysis_info['TR']
+high_pass_threshold = analysis_info['high_pass_threshold'] 
+high_pass_type = analysis_info['high_pass_type'] 
+sessions = analysis_info['session']
+hemis = ['L','R']
+
+
+task = 'SacLoc'
+session = 'ses-02'
+runs = ['01','02']
+hemis = ['L','R']
+
+
+# Datas end events directories
+event_dir = '{}/{}/{}/{}/func/'.format(main_dir,project_dir,subject,session)
+fsnative_bold_dir = '{}/{}/derivatives/pp_data/{}/func/fmriprep_dct/fsnative'.format(main_dir,project_dir,subject)
+score_out_dir = '{}/{}/derivatives/pp_data/{}/func/fmriprep_dct_decode_scores'.format(main_dir,project_dir,subject)
+os.makedirs(score_out_dir, exist_ok=True)
+
+# make an object with the events
+# dim 1 = runs / dim 2 = TRs / dim 3 = eyemov_amplitude, eyemov_direction
+stacked_events = [] 
+for n_run, run in enumerate(runs) :
+    event_file = glob.glob("{}/{}_{}_task-{}_run-{}_events.tsv".format(event_dir,subject,session,task,run))
+    event_data = pd.read_table(event_file[0])
+    eyemov_amplitude = np.array(event_data.eyemov_amplitude)
+    eyemov_direction = np.array(event_data.eyemov_direction)
+    amp_dir = np.column_stack((eyemov_amplitude, eyemov_direction))
+    stacked_events.append(amp_dir)  
+    
+events_runs = np.stack(stacked_events, axis=0)
+
+# Convert events in binary 0 = no eyes mvt 1 = eyes mvt
+binar_events_runs = np.where(events_runs[:,:,1] == 33, 0, 1)
+
+
+
+# Load BOLD data 
+stacked_data_L = []
+stacked_data_R = []
+
+for n_run, run in enumerate(runs) :
+    print(run)
+    data_name_L = "{}/{}_{}_task-{}_run-{}_hemi-L_space-fsnative_bold_dct.func.gii".format(fsnative_bold_dir,subject,session,task,run)
+    data_file_L = glob.glob(data_name_L)
+    
+    data_name_R = "{}/{}_{}_task-{}_run-{}_hemi-R_space-fsnative_bold_dct.func.gii".format(fsnative_bold_dir,subject,session,task,run)
+    data_file_R = glob.glob(data_name_R)
+    
+    img_L = nb.load(data_file_L[0])
+    data_L = [x.data for x in img_L.darrays]
+    data_L = np.vstack(data_L) 
+    header_L = img_L.header
+    meta_L = img_L.meta
+    print('hemi L done')
+
+    
+    img_R = nb.load(data_file_R[0])
+    data_R = [x.data for x in img_R.darrays]
+    data_R = np.vstack(data_R) 
+    header_R = img_L.header
+    meta_R = img_L.meta
+
+    stacked_data_L.append(data_L)
+    stacked_data_R.append(data_R)
+    print('hemi R done')
+    
+
+data_runs_L = np.stack(stacked_data_L, axis=0) 
+data_runs_R = np.stack(stacked_data_R, axis=0) 
+
+
+param_dist = {
+    'C': loguniform(1e-4, 1),
+    'kernel' : ["linear", "poly", "rbf", "sigmoid"], 
+    'gamma' : ['scale', 'auto']}
+ 
+
+for hemi in hemis : 
+    train_accuracy_verts = []  
+    test_accuracy_verts = [] 
+    
+    out_file_name_train = '{}/{}_task-{}_hemi-{}_fmriprep_mcl_train_score_bold.func.gii'.format(score_out_dir,subject,task,hemi)
+    
+    out_file_name_test = '{}/{}_task-{}_hemi_{}_fmriprep_mcl_test_score_bold.func.gii'.format(score_out_dir,subject,task,hemi)
+    
+    
+    if hemi == 'L':
+        data_runs_hemi = data_runs_L
+        header = header_L
+        meta = meta_L
+    elif hemi == 'R' :
+        data_runs_hemi = data_runs_R
+        header = header_R
+        meta = meta_R
+        
+    for vert in range(data_runs_hemi.shape[2]):
+
+        # split datas 
+        X_train = data_runs_hemi[0, :, vert].reshape(-1, 1)
+        X_test = data_runs_hemi[1, :, vert].reshape(-1, 1)
+        y_train = binar_events_runs[0, :]
+        y_test = binar_events_runs[1, :]
+
+        # radom search to optimise 
+        random_search = RandomizedSearchCV(SVC(), param_distributions=param_dist, n_iter=150, scoring='accuracy', random_state=42)
+        random_search.fit(X_train, y_train)
+
+        best_model = random_search.best_estimator_
+
+        best_y_pred_train= best_model.predict(X_train)
+        best_y_pred_test = best_model.predict(X_test)
+
+        best_accuracy_score_train = accuracy_score(best_y_pred_train, y_train)
+        best_accuracy_score_test = accuracy_score(best_y_pred_test, y_test)
+
+        train_accuracy_verts.append(best_accuracy_score_train)
+        test_accuracy_verts.append(best_accuracy_score_test)
+
+    train_accuracy_verts = np.array(train_accuracy_verts)
+    test_accuracy_verts = np.array(test_accuracy_verts)
+
+    train_accuracy_verts_img = nb.gifti.GiftiImage(header = header, meta = meta)
+    for data in train_accuracy_verts:
+        train_accuracy_verts_darray = nb.gifti.GiftiDataArray(data)
+        train_accuracy_verts_img.add_gifti_data_array(train_accuracy_verts_darray)
+
+    nb.save(train_accuracy_verts_img, out_file_name_train)
+
+    test_accuracy_verts_img = nb.gifti.GiftiImage(header = header, meta = meta)
+    for data in train_accuracy_verts:
+        test_accuracy_verts_darray = nb.gifti.GiftiDataArray(data)
+        test_accuracy_verts_img.add_gifti_data_array(test_accuracy_verts_darray)
+
+    nb.save(test_accuracy_verts_img, out_file_name_test)
+
+
+
+
+
--- analysis_code/postproc/prf/fit/prf_fit_surf.py
+++ analysis_code/postproc/prf/fit/prf_fit_surf.py
@@ -18,7 +18,7 @@ Nifti image files with fit parameters for a z slice
 -----------------------------------------------------------------------------------------
 To run :
 >> cd to function directory
-cd ~/projects/amblyo_prf/analysis_code/postproc/
+cd ~/projects/RetinoMaps/analysis_code/postproc/prf/fit/
 >> python prf/fit/prf_fit.py [subject] [timeseries] [visual design] 
                      [fit] [prediction] [nb_procs]
 -----------------------------------------------------------------------------------------
@@ -44,7 +44,7 @@ deb = ipdb.set_trace
 # from prfpy.timecourse import *
 from prfpy.stimulus import PRFStimulus2D
 from prfpy.model import Iso2DGaussianModel,Norm_Iso2DGaussianModel
-from prfpy.fit import Iso2DGaussianFitter
+from prfpy.fit import Iso2DGaussianFitter, Norm_Iso2DGaussianFitter
 import nibabel as nb
 
 # Get inputs
@@ -82,88 +82,124 @@ TR = analysis_info['TR']
 grid_nr = analysis_info['grid_nr']
 max_ecc_size = analysis_info['max_ecc_size']
 
+
+
 # Get task specific visual design matrix
 vdm = np.load(input_vd)
 
-if input_fn_HCP in locals(): 
+if 'input_fn_HCP' in vars(): 
 
 
     # Load HCP 170k data 
     data_img_HCP = nb.load(input_fn_HCP)
     data_HCP = data_img_HCP.get_fdata()
+    data_HCP = data_HCP[:,10:11 ] # juste for test
+    data_HCP = data_HCP.T # juste for test
     
-    data_var_HCP = np.var(data_HCP,axis=-1)
-    mask_HCP = data_var_HCP!=0.0    
-    num_vox_HCP = mask_HCP[...].sum()
-    data_to_analyse_HCP = data_HCP[mask_HCP]
-    data_where_HCP = np.where(data_var_HCP!=0.0)
-    data_indices_HCP = []
-    
-    for x,y in zip(data_where_HCP[0],data_where_HCP[1]):
-        data_indices_HCP.append((x,y))
+
+        
+        
+        
     fit_mat_gauss_HCP = np.zeros((data_HCP.shape[0],data_HCP.shape[1],6))
     pred_mat_gauss_HCP = np.zeros(data_HCP.shape)
     
     fit_mat_DN_HCP = np.zeros((data_HCP.shape[0],data_HCP.shape[1],6))
     pred_mat_DN_HCP = np.zeros(data_HCP.shape)
     
-    # determine model
+    # determine gauss model
     stimulus = PRFStimulus2D(screen_size_cm=screen_size_cm[1], 
                              screen_distance_cm=screen_distance_cm,
                              design_matrix=vdm, 
                              TR=TR)
     
-    
-    
-    DN_model = Norm_Iso2DGaussianModel(stimulus=stimulus)
     gauss_model = Iso2DGaussianModel(stimulus=stimulus)
     sizes = max_ecc_size * np.linspace(0.1,1,grid_nr)**2
     eccs = max_ecc_size * np.linspace(0.25,1,grid_nr)**2
     polars = np.linspace(0, 2*np.pi, grid_nr)
     
     # grid fit
-    print("Grid fit")
-    gauss_fitter = Iso2DGaussianFitter(data=data_to_analyse_HCP, model=gauss_model, n_jobs=nb_procs)
-    gauss_fitter.grid_fit(ecc_grid=eccs, polar_grid=polars, size_grid=sizes)
+    print("Grid fit Gauss")
+    gauss_fitter = Iso2DGaussianFitter(data=data_HCP, model=gauss_model, n_jobs=nb_procs)
+    gauss_fitter.grid_fit(ecc_grid=eccs, polar_grid=polars, size_grid=sizes, verbose=True)
     
-    DN_fitter = Iso2DGaussianFitter(data=data_to_analyse_HCP, model=DN_model, n_jobs=nb_procs)
-    DN_fitter.grid_fit(ecc_grid=eccs, polar_grid=polars, size_grid=sizes)
-    
+ 
     # iterative fit
-    print("Iterative fit")
-    gauss_fitter.iterative_fit(rsq_threshold=0.0001, verbose=False)
+    print("Iterative fit Gauss")
+    gauss_fitter.iterative_fit(rsq_threshold=0.0001, verbose=True)
     fit_fit_gauss_HCP = gauss_fitter.iterative_search_params
     
+    
+    
+    # determine DN model
+    DN_model = Norm_Iso2DGaussianModel(stimulus=stimulus)
+    
+    DN_fitter = Norm_Iso2DGaussianFitter(data=data_HCP, model=DN_model, n_jobs=nb_procs,
+                                         use_previous_gaussian_fitter_hrf=True,
+                                         previous_gaussian_fitter=gauss_fitter)
+                                         
+    
+    
+    # grid fit
+    print("Grid fit DN")
+    num = 7
+    fixed_grid_baseline=0
+    norm_grid_bounds = [(0,1000),(0,1000)] 
+    DN_fitter.grid_fit(surround_amplitude_grid=np.linspace(0,10,num),
+             surround_size_grid=np.linspace(1,10,num),
+             neural_baseline_grid=np.linspace(0,10,num),
+             surround_baseline_grid=np.linspace(1,10,num),
+             fixed_grid_baseline=fixed_grid_baseline,
+             grid_bounds=norm_grid_bounds,
+             hrf_1_grid=np.linspace(0,10,num),
+             hrf_2_grid=np.linspace(0,0,1),
+             # ecc_grid=eccs,
+             # polar_grid=polars,
+             # size_grid=sizes
+             )
+    
+    
+    
+    
+     
+    # iterative fit
+    print("Iterative fit Gauss")
     DN_fitter.iterative_fit(rsq_threshold=0.0001, verbose=False)
     fit_fit_DN_HCP = DN_fitter.iterative_search_params
     
+    print("it s okk !!!!!!!")
+
+    
+    deb()
     
     # Re-arrange data
-    for est,vox in enumerate(data_indices_HCP):
-        fit_mat_gauss_HCP[vox] = fit_fit_gauss_HCP[est]
-        pred_mat_gauss_HCP[vox] = gauss_model.return_prediction(  mu_x=fit_fit_gauss_HCP[est][0], mu_y=fit_fit_gauss_HCP[est][1], size=fit_fit_gauss_HCP[est][2], 
-                                                        beta=fit_fit_gauss_HCP[est][3], baseline=fit_fit_gauss_HCP[est][4])
+    for vert in range(len(fit_mat_gauss_HCP)):
+        fit_mat_gauss_HCP[vert] = fit_fit_gauss_HCP[vert]
+        pred_mat_gauss_HCP[vert] = gauss_model.return_prediction(  mu_x=fit_fit_gauss_HCP[vert][0], mu_y=fit_fit_gauss_HCP[vert][1], size=fit_fit_gauss_HCP[vert][2], 
+                                                        beta=fit_fit_gauss_HCP[vert][3], baseline=fit_fit_gauss_HCP[vert][4])
     
     # Re-arrange data
-    for est,vox in enumerate(data_indices_HCP):
-        fit_mat_DN_HCP[vox] = fit_fit_DN_HCP[est]
-        pred_mat_DN_HCP[vox] = DN_model.return_prediction(  mu_x=fit_fit_DN_HCP[est][0], mu_y=fit_fit_DN_HCP[est][1], size=fit_fit_DN_HCP[est][2], 
-                                                        beta=fit_fit_DN_HCP[est][3], baseline=fit_fit_DN_HCP[est][4])
+    for vert in range(len(fit_mat_DN_HCP)):
+        fit_mat_DN_HCP[vert] = fit_fit_gauss_HCP[vert]
+        pred_mat_DN_HCP[vert] = gauss_model.return_prediction(  mu_x=fit_fit_DN_HCP[vert][0], mu_y=fit_fit_DN_HCP[vert][1], size=fit_fit_DN_HCP[vert][2], 
+                                                        beta=fit_fit_DN_HCP[vert][3], baseline=fit_fit_DN_HCP[vert][4])
     
     
-    
+    print('export started',fit_fn_HCP)
     # export HCP DN
     fit_img_DN_HCP = nb.cifti2.cifti2.Cifti2Image(dataobj=fit_mat_DN_HCP, header=data_img_HCP.header)
     nb.save(fit_img_DN_HCP, fit_fn_HCP)
     
+    print('export started',pred_fn_HCP)
     pred_img_DN_HCP = nb.cifti2.cifti2.Cifti2Image(dataobj=pred_mat_DN_HCP, header=data_img_HCP.header)
     nb.save(pred_img_DN_HCP, pred_fn_HCP)
     
-    
+    print('export started',fit_fn_HCP)
     # export HCP gauss
     fit_img_gauss_HCP = nb.cifti2.cifti2.Cifti2Image(dataobj=fit_mat_gauss_HCP, header=data_img_HCP.header)
     nb.save(fit_img_gauss_HCP, fit_fn_HCP)
     
+    
+    print('export started',pred_fn_HCP)
     pred_img_gauss_HCP = nb.cifti2.cifti2.Cifti2Image(dataobj=pred_mat_gauss_HCP, header=data_img_HCP.header)
     nb.save(pred_img_gauss_HCP, pred_fn_HCP)
     
@@ -174,9 +210,3 @@ if input_fn_HCP in locals():
     print("\nStart time:\t{start_time}\nEnd time:\t{end_time}\nDuration:\t{dur}".format(
         start_time=start_time, end_time=end_time, dur=end_time - start_time))
 
-
-elif input_fn_fsnative in locals(): 
-    #start with fsnative
-    
-else : 
-    print('problems')
